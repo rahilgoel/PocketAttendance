37ee03796f5d993ca0ae8b701741eff325008a8f
rahilgoel <Mac@Mavericks-7.local>
2016-06-29 16:15:17 -0500

    first commit

---
+++ app/src/main/java/iit/iitr/pocketattendance/AllData copy.java
@@ -0,0 +1,52 @@
+package iit.iitr.pocketattendance;
+
+/**
+ * Created by Mac on 6/4/16.
+ */
+public class AllData {
+    String name;
+    String id;
+    String day;
+    String time;
+    String status;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
